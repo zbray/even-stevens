842c5b1e92ba6f0f1ba20d6a86aea0db50b808a8
Yagmur Guzeldereli <ms.guzeldereli@gmail.com>
2019-01-23 21:22:22 -0600

    eve2

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,37 +1,52 @@
-$("#search").on("click", function(event) {
-    event.preventDefault();
-    var jobSearch = $("input").val();
+$("#submit").on("click", function(event) {
+    alert("hello");
+    
+    var jobSearch = $("#jobValue").val().trim();
     var queryURL = "https://authenticjobs.com/api/" + jobSearch + "?api_key=95681d175f84e545da6da7ac528eb17a&method=aj.jobs.get&id=1569";
-    var box= $("<tr>")
-    var jobTitleTable= $("<td>Text</td>")
-    var cityTable= $("<td>Text</td>")
-    var descriptionTable= $("<td>Text</td>")
-    var salaryTable =$("<td>Text</td>")
-    
-    box.append(jobTitleTable)
-    box.append(cityTable)
-    box.append(descriptionTable)
-    box.append(salaryTable)
-    
-    $("table").append(box)
-    
     $.ajax({
         url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        $("#jobs").text(JSON.stringify(response));
-      });
-    });
+           method: "GET"
+          }).then(function(response) {
+           $("#resultsCard").text(JSON.stringify(response));
+});
+});
 
-    $("#search").on("click", function(event) {
+
+     $("#submit").on("click", function(event) {
         event.preventDefault();
-        var WeatherAp = $("input").val();
-        var queryURL = "https://openweathermap.org/api/" + WeatherAp + "?api_key=2839d424c20e5a81965724e469b669bb";
-        
-        $.ajax({
-            url: queryURL,
+        var zipInput = $("#zipValue").val();
+
+        var APIKey= "2839d424c20e5a81965724e469b669bb";
+        var weatherSearch = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipInput +
+      ",us&appid=" + APIKey;
+
+
+         $.ajax({
+            url: weatherSearch,
             method: "GET"
           }).then(function(response) {
             $("#weather").text(JSON.stringify(response));
+            
           });
+          console.log(response);
         });
+
+
+
+
+            // var box= $("<tr>")
+    // var jobTitleTable= $("<td>Text</td>")
+    // var cityTable= $("<td>Text</td>")
+    // var descriptionTable= $("<td>Text</td>")
+    // var salaryTable =$("<td>Text</td>")
+
+    //  box.append(jobTitleTable)
+    // box.append(cityTable)
+    // box.append(descriptionTable)
+    // box.append(salaryTable)
+
+    //  $("table").append(box)
+
+  
+    //   });
+    // });
